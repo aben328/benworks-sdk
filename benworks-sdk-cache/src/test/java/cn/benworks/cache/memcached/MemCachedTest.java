c0e271a7577b64f6ef9f9f9b38eebb9033b26910
aben328 <aben328@gmail.com>
2013-04-15 23:01:30 +0800

    测试是否可以提交

---
+++ benworks-sdk-cache/src/test/java/cn/benworks/cache/memcached/MemCachedTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package cn.benworks.cache.memcached;
+
+/**
+ * @author Ben
+ */
+public class MemCachedTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
